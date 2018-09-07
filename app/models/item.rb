75e51898483e6ad14959194db7ff936e1caf761f
y.k <kishinoyousuke@mail.goo.ne.jp>
2018-09-07 16:42:27 +0000

    have

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,11 @@ class Item < ApplicationRecord
   has_many :ownerships
   has_many :users, through: :ownerships
   
+  has_many :haves, class_name: 'Have'
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
+  
   has_many :wants
   has_many :want_users, through: :wants, class_name: 'User', source: :user
+  
+  
 end
